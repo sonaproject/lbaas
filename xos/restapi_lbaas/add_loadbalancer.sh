ba2d980baabbc0017f7f5461bdea3aecd9f1236d
andrewjeong0111 <andrewje@naver.com>
2017-08-18 12:03:07 +0900

    delete vip_address

--- xos/restapi_lbaas/add_loadbalancer.sh
+++ xos/restapi_lbaas/add_loadbalancer.sh
@@ -13,7 +13,6 @@ DATA=$(cat <<EOF
 {
     "name": "sona_loadbalancer",
     "vip_subnet_id": 1,
-    "vip_address": "0.0.0.0",
     "description": "web server loadbalancer"
 }
 EOF
@@ -30,25 +29,11 @@ DATA=$(cat <<EOF
     "listener": "$LISTENER_ID",
     "pool": "$POOL_ID",
     "vip_subnet_id": 1,
-    "vip_address": "0.0.0.0",
     "description": "web server loadbalancer"
 }
 EOF
 ) 
 
 fi
-
-
-<< "COMMENT"
-DATA=$(cat <<EOF
-{
-    "name": "sona_loadbalancer",
-    "vip_subnet_id": "013d3059-87a4-45a5-91e9-d721068ae0b2",
-    "vip_address": "10.6.1.198",
-    "description": "web server loadbalancer100"
-}
-EOF
-)
-COMMENT
 
 curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH -X POST -d "$DATA" $HOST/api/tenant/loadbalancers/
