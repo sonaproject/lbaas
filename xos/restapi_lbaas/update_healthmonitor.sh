ed1cb197e350fab92be377c1d7794a1e367ebf12
andrewjeong0111 <andrewje@naver.com>
2017-08-22 17:46:01 +0900

    initial commit

---
+++ xos/restapi_lbaas/update_healthmonitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./config.sh
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Syntax: $0 <health_monitor_id>"
+    exit -1
+fi
+
+HEALTH_ID=$1
+
+DATA=$(cat <<EOF
+{
+    "type": "TCP"
+}
+EOF
+)
+
+curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH -X PUT -d "$DATA" $HOST/api/tenant/healthmonitors/$HEALTH_ID/
