c958a4c45b5f7db801d203a8914bb6c4742ed7a4
andrewjeong0111 <andrewje@naver.com>
2017-08-23 16:02:01 +0900

    add pool data without health_monitor_id

--- xos/restapi_lbaas/add_pool.sh
+++ xos/restapi_lbaas/add_pool.sh
@@ -3,11 +3,21 @@
 source ./config.sh
 
 if [[ "$#" -ne 1 ]]; then
-    echo "Syntax: $0 <health_monitor_id>"
-    exit -1
-fi
+    echo "Syntax: $0"
 
-HEALTH_ID=$1
+DATA=$(cat <<EOF
+{
+    "name": "sona_pool",
+    "lb_algorithm": "ROUND_ROBIN",
+    "protocol": "HTTP",
+    "description": "sona_pool"
+}
+EOF
+)
+
+else
+    echo "Syntax: $0 <health_monitor_id>"
+    HEALTH_ID=$1
 
 DATA=$(cat <<EOF
 {
@@ -20,4 +30,5 @@ DATA=$(cat <<EOF
 EOF
 )
 
+fi
 curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH -X POST -d "$DATA" $HOST/api/tenant/pools/
