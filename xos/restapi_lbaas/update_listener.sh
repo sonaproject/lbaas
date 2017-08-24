f0b6ac7a951b8d275fb31171a18c9c8f5e8dfeab
andrewjeong0111 <andrewje@naver.com>
2017-08-24 14:53:05 +0900

    initial commit

---
+++ xos/restapi_lbaas/update_listener.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./config.sh
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Syntax: $0 <listener_id>"
+    exit -1
+fi
+
+LISTENER_ID=$1
+
+DATA=$(cat <<EOF
+{
+    "protocol": "HTTP"
+}
+EOF
+)
+
+curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH -X PUT -d "$DATA" $HOST/api/tenant/listeners/$LISTENER_ID/
