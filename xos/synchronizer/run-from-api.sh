4e3d7cd14c944312f830714b28de626a106002fe
andrewjeong0111 <andrewje@naver.com>
2017-09-11 21:10:07 +0900

    Implemented to run again when the process terminates

--- xos/synchronizer/run-from-api.sh
+++ xos/synchronizer/run-from-api.sh
@@ -1,2 +1,29 @@
-python lbaas-synchronizer.py &
-python sync_config.py &
+#!/bin/bash
+PROC1="python lbaas-synchronizer.py"
+PROC2="python sync_config.py"
+
+$PROC1 &
+$PROC2 &
+
+sleep 60
+
+while :
+do
+    RESULT=`ps -ef | grep "$PROC1" | grep -v 'grep'`
+    if [ "${RESULT:-null}" = null ]; then
+        echo "${PROC1} not running, starting "$PROC1
+        $PROC1 &
+    else
+        echo "${PROC1} running"
+    fi
+
+    RESULT=`ps -ef | grep "$PROC2" | grep -v 'grep'`
+    if [ "${RESULT:-null}" = null ]; then
+        echo "${PROC2} not running, starting "$PROC2
+        $PROC2 &
+    else
+        echo "${PROC2} running"
+    fi
+
+    sleep 10
+done
